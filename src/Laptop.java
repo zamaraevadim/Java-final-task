1526ca7e733e21a64f4b4705f5f3de312787d317
Пользователь <zamaraevadim@yandex.ru>
2023-11-20 22:29:11 +0500

    Create constructor, methods

--- src/Laptop.java
+++ src/Laptop.java
@@ -1,2 +1,45 @@
 public class Laptop {
+    private int ram;
+    private int HDD;
+    private String OS;
+    private String Color;
+
+    public Laptop(int ram, int HDD, String OS, String color) {
+        this.ram = ram;
+        this.HDD = HDD;
+        this.OS = OS;
+        Color = color;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
+    public int getHDD() {
+        return HDD;
+    }
+
+    public void setHDD(int HDD) {
+        this.HDD = HDD;
+    }
+
+    public String getOS() {
+        return OS;
+    }
+
+    public void setOS(String OS) {
+        this.OS = OS;
+    }
+
+    public String getColor() {
+        return Color;
+    }
+
+    public void setColor(String color) {
+        Color = color;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
 public class Main {
+    public static void main(String[] args) {
+        Laptop laptop1 = new Laptop(16,1000,"Windows","White");
+        Laptop laptop2 = new Laptop(32,500,"macOS","Black");
+        Laptop laptop3 = new Laptop(16,2000,"Windows","Green");
+        Laptop laptop4 = new Laptop(8,1500,"macOS","Blue");
+        Laptop laptop5 = new Laptop(64,1000,"Windows","White");
+        Laptop laptop6 = new Laptop(8,100,"macOS","White");
+    }
 }
